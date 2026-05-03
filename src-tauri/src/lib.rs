9474121c9b0e49be984a77d1ca29b05f3ac1a2dc
Arthur Manenti <arthur.manenti.videira@gmail.com>
2026-05-03 11:43:05 -0300

    Primeiro teste de integração entre o React e o Rust

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,15 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+
+#[tauri::command] //Define que estas funções vão para o frontend
+fn saudacao_personalizada(name: &str) -> String{
+    format!("Olá, {}! Este comando veio direto do Rust 🦀", name)
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![saudacao_personalizada])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
